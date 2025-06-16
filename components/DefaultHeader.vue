bf40f9d2caaf3e07abf7d2d246b594b9c8605c46
R1khsi <rikhsimboyev1997@gmail.com>
2025-06-16 05:54:20 +0500

    initial structure

---
+++ components/DefaultHeader.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>1dsfsf</div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="layout">
+        <header class="header">
+            <DefaultHeader/>
+        </header>
+
+        <main class="main">
+            <slot/>
+        </main>
+
+        <footer class="footer">
+            <DefaultFooter />
+        </footer>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+    .layout {
+        display: flex;
+        flex-direction: column;
+        min-height: 100svh;
+    }   
+
+    .main {
+        flex: 1;
+    }
+</style>
